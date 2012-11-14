0bf846268338f510e214b1b6fb3fe31c1a197ed5
Pierre Chambart <pierre.chambart@ocamlpro.org>
2012-11-14 10:58:06 +0100

    Add headers and COPYING.txt

--- src/read_string.ml
+++ src/read_string.ml
@@ -1,3 +1,20 @@
+(************************************************************************)
+(*  ocp-read                                                            *)
+(*                                                                      *)
+(*    Copyright 2012 OCamlPro                                           *)
+(*                                                                      *)
+(*  This file is distributed under the terms of the GNU Lesser General  *)
+(*  Public License as published by the Free Software Foundation; either *)
+(*  version 2.1 of the License, or (at your option) any later version,  *)
+(*  with the OCaml static compilation exception.                        *)
+(*                                                                      *)
+(*  ocp-read is distributed in the hope that it will be useful,         *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of      *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *)
+(*  GNU General Public License for more details.                        *)
+(*                                                                      *)
+(************************************************************************)
+
 let sign8 v =
   (v lsl ( Sys.word_size - 9 )) asr ( Sys.word_size - 9 )
 
