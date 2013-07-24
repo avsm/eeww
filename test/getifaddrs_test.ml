a308fd353549ffd9d4028ba3b0e32bec890fff1b
David Sheets <sheets@alum.mit.edu>
2013-07-24 18:20:58 +0100

    Switch from cstruct to ipaddr

--- test/getifaddrs_test.ml
+++ test/getifaddrs_test.ml
@@ -1,9 +1,11 @@
 open Tuntap
-open Cstruct
+module Ipv4 = Ipaddr.V4
 
 let () =
   let addrs = getifaddrs () in
   List.iter (fun (n, ifa) ->
-    Printf.printf "%s -> %s, %s, %s\n%!"
-      n (ipv4_to_string ifa.addr) (ipv4_to_string ifa.mask) (ipv4_to_string ifa.brd)
+    Printf.printf "%s -> %s, %s, %s\n%!" n
+      (Ipv4.to_string ifa.addr)
+      (Ipv4.to_string ifa.mask)
+      (Ipv4.to_string ifa.brd)
   ) addrs
