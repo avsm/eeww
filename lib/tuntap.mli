ed07fd038b5720bdb0aed7b41c2fc97b9ab66550
Anil Madhavapeddy <anil@recoil.org>
2013-10-12 15:18:04 +0100

    format via ocp-indent, add copyright

--- lib/tuntap.mli
+++ lib/tuntap.mli
@@ -1,3 +1,20 @@
+(*
+ * Copyright (c) 2010-2013 Anil Madhavapeddy <anil@recoil.org>
+ * Copyright (c) 2013 Vincent Bernardoff <vb@luminar.eu.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
 (** Module for dealing with TUN/TAP devices. TUN refers to layer 3
     virtual interfaces whereas TAP refers to layer 2 ones. *)
 
@@ -27,8 +44,8 @@ val closetap : string -> unit
 
 val get_ifnamsiz : unit -> int
 (** [get_ifnamsiz ()] is the value of the constant IFNAMSIZ,
-		defined in <net/if.h>. Useful for allocating buffers that contain
-		interface names. *)
+    defined in <net/if.h>. Useful for allocating buffers that contain
+    interface names. *)
 
 val get_macaddr : string -> Macaddr.t
 (** [get_hwaddr devname] is the MAC address of interface
@@ -43,12 +60,11 @@ val set_up_and_running : string -> unit
     running. Note that when using the [set_ipv4] function, the
     interface will automatically be set up and running. *)
 
-type iface_addr =
-    {
-      addr: Ipaddr.V4.t;
-      mask: Ipaddr.V4.t;
-      brd:  Ipaddr.V4.t;
-    }
+type iface_addr = {
+  addr: Ipaddr.V4.t;
+  mask: Ipaddr.V4.t;
+  brd:  Ipaddr.V4.t;
+}
 (** Type of the interface addresses record. *)
 
 val getifaddrs : unit -> (string * iface_addr) list
