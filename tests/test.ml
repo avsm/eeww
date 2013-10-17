ccc03c46fec765b6b381b28a73dc1b273e693b9c
Pierre Chambart <pierre.chambart@ocamlpro.org>
2013-10-17 13:43:41 +0200

    return code in tests

--- tests/test.ml
+++ tests/test.ml
@@ -4,14 +4,27 @@ let allocdiff =
   let stat2 = Gc.quick_stat () in
   (stat2.Gc.minor_words -. stat1.Gc.minor_words)
 
+let version_over_4_01 =
+  try
+    Scanf.sscanf Sys.ocaml_version "%i.%i.%i"
+      (fun major minor patch -> major > 4 || (major = 4 && minor >= 1))
+  with _ -> false
+
 let () =
   Test_bigstring.test1 ();
   let stat1 = Gc.quick_stat () in
   Test_bigstring.test2 ();
   let stat2 = Gc.quick_stat () in
-  Printf.printf "bigstring: allocated words %f\n%!" (stat2.Gc.minor_words -. stat1.Gc.minor_words -. allocdiff);
+  let alloc1 = stat2.Gc.minor_words -. stat1.Gc.minor_words -. allocdiff in
+  Printf.printf "bigstring: allocated words %f\n%!" alloc1;
   Test_string.test1 ();
   let stat1 = Gc.quick_stat () in
   Test_string.test2 ();
   let stat2 = Gc.quick_stat () in
-  Printf.printf "string: allocated words %f\n%!" (stat2.Gc.minor_words -. stat1.Gc.minor_words -. allocdiff)
+  let alloc2 = stat2.Gc.minor_words -. stat1.Gc.minor_words -. allocdiff in
+  Printf.printf "string: allocated words %f\n%!" alloc2;
+  (* we cannot ensure that there are no allocations only with the
+     primives added in 4.01.0 *)
+  if version_over_4_01 && (alloc1 <> 0. || alloc2 <> 0.)
+  then exit 1
+  else exit 0
