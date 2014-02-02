668261f3be7e1b5877703277fbfbe059304399c0
Vincent Bernardoff <vb@luminar.eu.org>
2014-02-02 19:24:04 +0100

    Updated travis.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -12,7 +12,7 @@ esac
 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam gdb
 
 export OPAMYES=1
 export OPAMVERBOSE=1
@@ -27,4 +27,9 @@ opam install ${OPAM_DEPENDS}
 
 eval `opam config env`
 make
-./_build/test/getifaddrs_test.native
+cat > commands <<EOT
+run
+bt
+quit
+EOT
+gdb ./_build/test/getifaddrs_test.native -x commands
