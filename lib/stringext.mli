668e193c97479951bf2a75d27a3f5b1d2802a7f6
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-03-21 16:38:28 -0700

    add mli

---
+++ lib/stringext.mli
@@ -0,0 +1,22 @@
+(** Misc. string functions not found in the built in OCaml string
+    module *)
+
+(** string_after [s] [n] returns the substring of [s] that is after
+    character [n] *)
+val string_after : string -> int -> string
+
+(** equivalent to [Str.quote] *)
+val quote : string -> string
+
+(** split [?max] [s] [~on] splits [s] on every [on] occurence upto
+    [max] number of items if [max] is specified. [max] is assumed to
+    be a small number if specified. To not cause stack overflows *)
+val split : ?max:int -> string -> on:char -> string list
+
+(** Trims spaces on the left of the string *)
+val trim_left : string -> string
+
+(** split_strim_right [s] [~on] [~trim] splits [s] on every character
+    in [on]. Characters in [trim] are trimmed from the left of every
+    result element *)
+val split_trim_left : string -> on:string -> trim:string -> string list
