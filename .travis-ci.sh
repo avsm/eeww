813222baf22cd6e13cbb77a8fae619b92a78369c
Pierre Chambart <pierre.chambart@ocamlpro.com>
2014-05-25 22:17:59 +0200

    Fix travis build script: add dependency to bytes

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 # Edit this for your own project dependencies
-OPAM_DEPENDS="ocamlfind optcomp"
+OPAM_DEPENDS="ocamlfind optcomp bytes"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
