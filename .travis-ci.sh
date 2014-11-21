081ed1f7c629b517155c54e87e014bf81d3613a0
Peter Zotov <whitequark@whitequark.org>
2014-11-21 23:13:56 +0300

    Update Travis to move to opam 1.2 and add OCaml 4.02.1.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,9 +2,10 @@
 OPAM_DEPENDS="ocamlfind optcomp bytes"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
-3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+3.12.1,1.2.0) ppa=avsm/ocaml312+opam12 ;;
+4.00.1,1.2.0) ppa=avsm/ocaml40+opam12 ;;
+4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
+4.02.1,1.2.0) ppa=avsm/ocaml42+opam12 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
