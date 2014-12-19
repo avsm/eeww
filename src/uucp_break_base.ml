edf5708820a4e2b4c646e25241bd3cd39fce809b
Daniel Bünzli <daniel.buenzli@erratique.ch>
2014-12-19 10:37:40 +0100

    Change line break property byte assignement.

--- src/uucp_break_base.ml
+++ src/uucp_break_base.ml
@@ -12,24 +12,21 @@ type line =
   | `JT | `JV | `LF | `NL | `NS | `NU | `OP | `PO | `PR | `QU | `RI
   | `SA | `SG | `SP | `SY | `WJ | `XX | `ZW ]
 
-(* N.B. the order here matches the one of this table:
-   http://www.unicode.org/reports/tr14/#Table2 *)
-
 let line_of_byte : line array =
-  [| `OP; `CL; `CP; `QU; `GL; `NS; `EX; `SY; `IS; `PR; `PO; `NU; `AL; `HL;
-     `ID; `IN; `HY; `BA; `BB; `B2; `ZW; `CM; `WJ; `H2; `H3; `JL; `JV; `JT;
-     `RI; `AI; `BK; `CB; `CJ; `CR; ` LF; `NL; `SA; `SG; `SP; `XX |]
+  [| `AI; `AL; `B2; `BA; `BB; `BK; `CB; `CJ; `CL; `CM; `CP; `CR; `EX; `GL; `H2;
+     `H3; `HL; `HY; `ID; `IN; `IS; `JL; `JT; `JV; `LF; `NL; `NS; `NU; `OP; `PO;
+     `PR; `QU; `RI; `SA; `SG; `SP; `SY; `WJ; `XX; `ZW |]
 
 let line_max = Array.length line_of_byte - 1
 
 let line_to_byte = function
-| `OP -> 0 | `CL -> 1 | `CP -> 2 | `QU -> 3 | `GL -> 4 | `NS -> 5 | `EX -> 6
-| `SY -> 7 | `IS -> 8 | `PR -> 9 | `PO -> 10 | `NU -> 11 | `AL -> 12 | `HL -> 13
-| `ID -> 14 | `IN -> 15 | `HY -> 16 | `BA -> 17 | `BB -> 18 | `B2 -> 19
-| `ZW -> 20 | `CM -> 21 | `WJ -> 22 | `H2 -> 23 | `H3 -> 24 | `JL -> 25
-| `JV -> 26 | `JT -> 27 | `RI -> 28 | `AI -> 29 | `BK -> 30 | `CB -> 31
-| `CJ -> 32 | `CR -> 33 | `LF -> 34 | `NL -> 35 | `SA -> 36 | `SG -> 37
-| `SP -> 38 | `XX -> 39
+| `AI -> 0 | `AL -> 1 | `B2 -> 2 | `BA -> 3 | `BB -> 4 | `BK -> 5 | `CB -> 6
+| `CJ -> 7 | `CL -> 8 | `CM -> 9 | `CP -> 10 | `CR -> 11 | `EX -> 12
+| `GL -> 13| `H2 -> 14 | `H3 -> 15 | `HL -> 16 | `HY -> 17 | `ID -> 18
+| `IN -> 19 | `IS -> 20 | `JL -> 21 | `JT -> 22 | `JV -> 23 | `LF -> 24
+| `NL -> 25 | `NS -> 26 | `NU -> 27 | `OP -> 28 | `PO -> 29 | `PR -> 30
+| `QU -> 31 | `RI -> 32 | `SA -> 33 | `SG -> 34 | `SP -> 35 | `SY -> 36
+| `WJ -> 37 | `XX -> 38 | `ZW -> 39
 
 let pp_line ppf v = Format.fprintf ppf "%s" begin match v with
   | `AI -> "AI" | `AL -> "AL" | `B2 -> "B2" | `BA -> "BA" | `BB -> "BB"
