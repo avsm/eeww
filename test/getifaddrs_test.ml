fcff28efd0de4bae6fbe0cf1706dfb58d9045cc3
Vincent Bernardoff <vb@luminar.eu.org>
2015-01-09 10:53:11 +0100

    Fix test code for new interface.

--- test/getifaddrs_test.ml
+++ test/getifaddrs_test.ml
@@ -4,10 +4,10 @@ open Ipaddr
 let () =
   let addrs = getifaddrs () in
   List.iter (function
-      | `V4 (n, a, p) ->
+      | n, `V4 (a, p) ->
         Printf.printf "%s -> %s/%d\n"
           n (V4.to_string a) (V4.Prefix.bits p)
-      | `V6 (n, a, p) ->
+      | n, `V6 (a, p) ->
         Printf.printf "%s -> %s/%d\n"
           n (V6.to_string ~v4:false a) (V6.Prefix.bits p)
     ) addrs
