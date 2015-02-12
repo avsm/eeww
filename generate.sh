c487ba2891f4d8cc3b4079ea50bf9cdff0e89043
Maxence Guesdon <maxence.guesdon@inria.fr>
2015-02-12 15:24:12 +0100

    use more-mime.types and mime.types (in this order)

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ex
 # Generate the mime_types.ml file
 
-ocaml str.cma generate_mime_types.ml mime.types > mime_types.ml
+cat more-mime.types mime.types | ocaml str.cma generate_mime_types.ml > mime_types.ml
