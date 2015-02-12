feec09359db660dba7ce0093eb213ba3265cbb29
Maxence Guesdon <maxence.guesdon@inria.fr>
2015-02-12 15:28:54 +0100

    fix order of mime types spec files

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ex
 # Generate the mime_types.ml file
 
-cat more-mime.types mime.types | ocaml str.cma generate_mime_types.ml > mime_types.ml
+cat mime.types more-mime.types | ocaml str.cma generate_mime_types.ml > mime_types.ml
