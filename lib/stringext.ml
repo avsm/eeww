97c564903743991e995a4d384ac418d88078b9f8
Rudi Grinberg <rudi.grinberg@gmail.com>
2015-06-18 11:23:58 -0400

    Remove leading spaces and fix some exception messages

--- lib/stringext.ml
+++ lib/stringext.ml
@@ -148,7 +148,7 @@ let find_from ?(start=0) str ~pattern =
         raise (Found_int i)
     done;
     None
-  with 
+  with
   | Found_int i -> Some i
   |  _ -> None
 
@@ -246,11 +246,11 @@ let to_array s = Array.init (String.length s) (String.get s)
 (* ripped off from one of dbuenzli's libs *)
 let cut s ~on =
   let sep_max = length on - 1 in
-  if sep_max < 0 then invalid_arg "String.cut: empty separator" else
+  if sep_max < 0 then invalid_arg "Stringext.cut: empty separator" else
     let s_max = length s - 1 in
     if s_max < 0 then None else
       let k = ref 0 in
-      let i = ref 0 in 
+      let i = ref 0 in
       (* We run from the start of [s] to end with [i] trying to match the
          first character of [on] in [s]. If this matches, we verify that
          the whole [on] is matched using [k]. If it doesn't match we
@@ -260,32 +260,32 @@ let cut s ~on =
          after the [on] we found to end of string. If [i] is such that no
          separator can be found we exit the loop and return the no match
          case. *)
-      try 
+      try
         while (!i + sep_max <= s_max) do
           (* Check remaining [on] chars match, access to unsafe s (!i + !k) is
              guaranteed by loop invariant. *)
           if unsafe_get s !i <> unsafe_get on 0 then incr i else begin
-            k := 1; 
+            k := 1;
             while (!k <= sep_max && unsafe_get s (!i + !k) = unsafe_get on !k)
             do incr k done;
             if !k <= sep_max then (* no match *) incr i else raise Exit
           end
-        done; 
+        done;
         None (* no match in the whole string. *)
       with
-      | Exit -> (* i is at the beginning of the separator *) 
-        let left_end = !i - 1 in 
+      | Exit -> (* i is at the beginning of the separator *)
+        let left_end = !i - 1 in
         let right_start = !i + sep_max + 1 in
-        Some (sub s 0 (left_end + 1), 
+        Some (sub s 0 (left_end + 1),
               sub s right_start (s_max - right_start + 1))
 
 let rcut s ~on =
   let sep_max = length on - 1 in
-  if sep_max < 0 then invalid_arg "String.rcut: empty separator" else
+  if sep_max < 0 then invalid_arg "Stringext.rcut: empty separator" else
     let s_max = length s - 1 in
     if s_max < 0 then None else
       let k = ref 0 in
-      let i = ref s_max in 
+      let i = ref s_max in
       (* We run from the end of [s] to the beginning with [i] trying to
          match the last character of [on] in [s]. If this matches, we
          verify that the whole [on] is matched using [k] (we do that
@@ -295,10 +295,10 @@ let rcut s ~on =
          [on] we found and another from the position after the [on] we
          found to end of string.  If [i] is such that no separator can
          be found we exit the loop and return the no match case. *)
-      try 
+      try
         while (!i >= sep_max) do
-          if unsafe_get s !i <> unsafe_get on sep_max then decr i else begin 
-            (* Check remaining [on] chars match, access to unsafe_get 
+          if unsafe_get s !i <> unsafe_get on sep_max then decr i else begin
+            (* Check remaining [on] chars match, access to unsafe_get
                s (sep_start + !k) is guaranteed by loop invariant. *)
             let sep_start = !i - sep_max in
             k := sep_max - 1;
@@ -306,13 +306,13 @@ let rcut s ~on =
             do decr k done;
             if !k >= 0 then (* no match *) decr i else raise Exit
           end
-        done; 
+        done;
         None (* no match in the whole string. *)
       with
-      | Exit -> (* i is at the end of the separator *) 
-        let left_end = !i - sep_max - 1 in 
+      | Exit -> (* i is at the end of the separator *)
+        let left_end = !i - sep_max - 1 in
         let right_start = !i + 1 in
-        Some (sub s 0 (left_end + 1), 
+        Some (sub s 0 (left_end + 1),
               sub s right_start (s_max - right_start + 1))
 
 let chop_prefix s ~prefix =
