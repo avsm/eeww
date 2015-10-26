c3675450930d3168a1d61dfce1ef4aed0beb70b8
Daniel Bünzli <daniel.buenzli@erratique.ch>
2015-10-26 21:32:10 +0000

    Update topkg.

--- pkg/build.ml
+++ pkg/build.ml
@@ -3,11 +3,11 @@
 #use "topkg.ml"
 
 let () =
-  Pkg.describe "uucp" ~builder:`OCamlbuild [
+  Pkg.describe "uucp" ~builder:(`OCamlbuild []) [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src/uucp";
-    Pkg.doc "README.md"; 
-    Pkg.doc "CHANGES.md"; 
-    Pkg.doc "DEVEL.md"; 
+    Pkg.doc "README.md";
+    Pkg.doc "CHANGES.md";
+    Pkg.doc "DEVEL.md";
     Pkg.doc "test/examples.ml";
   ]
