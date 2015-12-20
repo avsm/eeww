d2e7c276b1922846f9eb7c669b34897f5428299c
Hannes Mehnert <hannes@mehnert.org>
2015-12-20 17:02:26 +0100

    fix for topkg update

--- pkg/build.ml
+++ pkg/build.ml
@@ -3,7 +3,7 @@
 #use "topkg.ml";;
 
 let alcotest = Env.bool "alcotest"
-let () = Pkg.describe "hkdf" ~builder:`OCamlbuild [
+let () = Pkg.describe "hkdf" ~builder:(`OCamlbuild []) [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "hkdf";
     Pkg.bin ~cond:alcotest ~auto:true "rfctests";
