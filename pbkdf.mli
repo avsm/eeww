2b756e168e49434227fb1276a0c174506008bbc2
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2016-03-14 11:38:59 +0100

    Add documentation for pbkdf2

--- pbkdf.mli
+++ pbkdf.mli
@@ -5,6 +5,8 @@ module type S = sig
   (** [pbkdf1 password salt count dk_len] is [dk], the derived key of [dk_len] octets.
       The [salt] must be eight octets, [count] the iteration count. *)
   val pbkdf1 : password:Cstruct.t -> salt:Cstruct.t -> count:int -> dk_len:int -> Cstruct.t
+
+  (** [pbkdf2 password salt count dk_len] is [dk], the derived key of [dk_len] octets. *)
   val pbkdf2 : password:Cstruct.t -> salt:Cstruct.t -> count:int -> dk_len:int -> Cstruct.t
 end
 
@@ -13,4 +15,6 @@ module Make (H: Nocrypto.Hash.S) : S
 
 (** convenience [pbkdf1 hash password salt count dk_len] where the [hash] has to be provided explicitly *)
 val pbkdf1 : hash:Nocrypto.Hash.hash -> password:Cstruct.t -> salt:Cstruct.t -> count:int -> dk_len:int -> Cstruct.t
+
+(** convenience [pbkdf2 prf password salt count dk_len] where the [prf] has to be provided explicitly *)
 val pbkdf2 : prf:Nocrypto.Hash.hash -> password:Cstruct.t -> salt:Cstruct.t -> count:int -> dk_len:int -> Cstruct.t
