c192a8a1b8c45583845e803c5786abf1de64cb7c
Hannes Mehnert <hannes@mehnert.org>
2016-10-03 11:59:16 +0100

    fix statistical distribution of Randomconv.int by not using mod

--- src/randomconv.ml
+++ src/randomconv.ml
@@ -11,20 +11,28 @@ let int32 g = Cstruct.LE.get_uint32 (g 4) 0
 
 let int64 g = Cstruct.LE.get_uint64 (g 8) 0
 
-let int ?(bound = max_int) g =
+let bitmask n =
+  let rec go c = function
+    | 0 -> c
+    | n -> go (c lsl 1) (n lsr 1)
+  in
+  go 1 n - 1
+
+let rec int ?(bound = max_int) g =
   if bound <= 1 then invalid_arg "bound smaller or equal 1 not supported" ;
   let r =
     if bound < 256 then
-      (Cstruct.get_uint8 (g 1) 0) mod bound
+      int8 g
     else if bound < 65536 then
-      (Cstruct.LE.get_uint16 (g 2) 0) mod bound
+      int16 g
     else
       match Sys.word_size with
-      | 32 -> (Int32.to_int (int32 g)) mod bound
-      | 64 -> (Int64.to_int (int64 g)) mod bound
+      | 32 -> Int32.to_int (int32 g)
+      | 64 -> Int64.to_int (int64 g)
       | _ -> invalid_arg "unknown word size"
   in
-  if r < 0 then r + bound else r
+  let r = r land bitmask (pred bound) in
+  if r < bound then r else int ~bound g
 
 let float ?(bound = 1.) g =
   if bound <= 0. then invalid_arg "bound smaller or equal 0 not supported" ;
