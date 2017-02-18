33d1cae2c084b462d7355d210beba3fe43e4d739
Hannes Mehnert <hannes@mehnert.org>
2017-02-18 10:45:13 +0000

    fix linking of native and bytecode libraries: -ltuntap_stubs is required for binaries
    remove `build-test` in opam, they don't work on the CI (require sudo)

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -15,6 +15,7 @@ let () =
     Pkg.clib "lib/libtuntap_stubs.clib";
     Pkg.test "test/getifaddrs_test";
     Pkg.test "test/nonblock_read";
+    Pkg.test "test/nonblock_test";
     Pkg.test "test/open_close_test";
     Pkg.test "test/sendfd_test";
     Pkg.test "test/set_ipv4_test";
