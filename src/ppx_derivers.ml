3e33627191efbff65a12084fdc426147a3790c1f
Jeremie Dimino <jdimino@janestreet.com>
2017-04-17 17:24:14 +0100

    initial import

---
+++ src/ppx_derivers.ml
@@ -0,0 +1,14 @@
+type deriver = ..
+
+let all = Hashtbl.create 42
+
+let register name deriver =
+  if Hashtbl.mem all name then
+    Printf.ksprintf failwith
+      "Ppx_deriviers.register: %S is already registered" name;
+  Hashtbl.add all name deriver
+
+let lookup name =
+  match Hashtbl.find all name with
+  | drv -> Some drv
+  | exception Not_found -> None
