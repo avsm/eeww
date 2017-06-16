58bf296e662c0b2a7bcc94f3cddfa4efb057e8a5
Thomas Gazagnaire <thomas@gazagnaire.org>
2017-06-16 16:09:55 +0200

    Add first-class flows

--- lwt/mirage_flow_lwt.mli
+++ lwt/mirage_flow_lwt.mli
@@ -142,3 +142,27 @@ module F: sig
   (** Same as {!strings} but for {!Cstruct.t} buffers. *)
 
 end
+
+type t
+(** The type for first-class flows. *)
+
+include S with type flow = t
+
+val create: (module S with type flow = 'a) -> 'a -> string -> t
+(** [create (module M) t name] is the flow representing [t] using the
+    function defined in [M]. *)
+
+val pp: t Fmt.t
+(** [pp] is the pretty-printer for IO flows. *)
+
+val forward: ?verbose:bool -> src:t -> dst:t -> unit Lwt.t
+(** [forward ?verbose ~src ~dst] forwards writes from [src] to
+    [dst]. Block until either [src] or [dst] is closed. If [verbose]
+    is set (by default it is not), show the full flow contents in the debug
+    messages. *)
+
+val proxy: ?verbose:bool -> t -> t -> unit Lwt.t
+(** [proxy ?verbose x y] is the same as [forward x y <*> forward y
+    x]. Block until both flows are closed. If [verbose] is set (by
+    default it is not), show the full flow contents in the debug
+    messages. *)
