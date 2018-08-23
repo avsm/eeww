2c8f293edd7e7ba4ed5cf46869ab1cc5d12c67fc
Daniel Bünzli <daniel.buenzli@erratique.ch>
2018-08-23 15:32:11 +0200

    Add Float module.

--- test/test.ml
+++ test/test.ml
@@ -1,8 +1,11 @@
 let _ = Stdlib.(+)
 let _ = Stdlib.List.map
 
-let _ = Uchar.min
-let _ = Stdlib.Uchar.min
+let _ = Float.max_float
+let _ = Stdlib.Float.max_float
 
 let _ = Seq.empty
 let _ = Stdlib.Seq.empty
+
+let _ = Uchar.min
+let _ = Stdlib.Uchar.min
