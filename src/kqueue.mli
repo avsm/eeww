6c504d8a11d5be689001da5cbfe7da4fb1500d63
Anurag Soni <anurag@sonianurag.com>
2020-08-14 15:42:22 -0400

    add timespec to public api

--- src/kqueue.mli
+++ src/kqueue.mli
@@ -1,3 +1,13 @@
-type t = int
+module Timespec : sig
+  type t [@@deriving sexp]
+
+  val sec : t -> int64
+
+  val nsec : t -> int64
+
+  val make : sec:int64 -> nsec:int64 -> t
+end
+
+type t
 
 val kqueue : unit -> t
