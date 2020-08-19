87fd8833530e52adbaca557a7b877170307c3b6b
Anurag Soni <anurag@sonianurag.com>
2020-08-19 19:37:25 -0400

    update kqueue echo server

--- example/echo_server.ml
+++ example/echo_server.ml
@@ -5,10 +5,15 @@ let unsafe_fd_to_int (t : Unix.file_descr) : int = Obj.magic t
 
 let unsafe_int_to_fd (t : int) : Unix.file_descr = Obj.magic t
 
+let lor_flags flags =
+  List.fold_left
+    (fun acc flag -> Unsigned.UInt16.logor acc flag)
+    Unsigned.UInt16.zero flags
+
 let run socket q =
   let b = Bytes.create 4096 in
   let changelist = CArray.make Bindings.Kevent.t 0 in
-  let eventlist = CArray.make Bindings.Kevent.t 1024 in
+  let eventlist = CArray.make Bindings.Kevent.t 64 in
   let rec run () =
     let n = kevent q ~changelist ~eventlist in
     if n = -1 then (
@@ -19,16 +24,17 @@ let run socket q =
       let ident = Kevent.ident event |> Uintptr.to_int in
       let flags = Kevent.flags event in
       (* Close if EOF *)
-      if Unsigned.UInt16.(logand flags Bindings.ev_eof > zero) then
+      if Unsigned.UInt16.(logand flags Bindings.ev_eof = Bindings.ev_eof) then
         Unix.close (unsafe_int_to_fd ident)
       else if unsafe_fd_to_int socket = ident then (
         let client_fd, _ = Unix.accept socket in
         Unix.set_nonblock client_fd ;
+        Unix.set_close_on_exec client_fd ;
         let client_ident = unsafe_fd_to_int client_fd |> Uintptr.of_int in
         let e =
           Kevent.make ~ident:client_ident ~filter:Bindings.evfilt_read
-            ~flags:(Unsigned.UInt16.logor Bindings.ev_add Bindings.ev_enable)
-            ~fflags:Unsigned.UInt32.zero ~data:Intptr.zero ~udata:Uintptr.zero
+            ~flags:Bindings.ev_add ~fflags:Unsigned.UInt32.zero
+            ~data:Intptr.zero ~udata:Uintptr.zero
         in
         let res =
           kevent q
@@ -38,8 +44,14 @@ let run socket q =
         assert (res > -1) )
       else if Kevent.filter event = Bindings.evfilt_read then
         try
-          let bc = Unix.read (unsafe_int_to_fd ident) b 0 4096 in
-          if bc > 0 then ignore (Unix.write (unsafe_int_to_fd ident) b 0 bc)
+          let client_fd = unsafe_int_to_fd ident in
+          let rec aux () =
+            let bc = Unix.read client_fd b 0 4096 in
+            if bc > 0 then (
+              ignore (Unix.write client_fd b 0 bc) ;
+              aux () )
+          in
+          aux ()
         with
         | Unix.Unix_error (Unix.EAGAIN, _, _)
         | Unix.Unix_error (Unix.EWOULDBLOCK, _, _)
@@ -66,8 +78,7 @@ let () =
   let ev =
     Kevent.make
       ~ident:(unsafe_fd_to_int socket |> Uintptr.of_int)
-      ~filter:Bindings.evfilt_read
-      ~flags:(Unsigned.UInt16.logor Bindings.ev_add Bindings.ev_enable)
+      ~filter:Bindings.evfilt_read ~flags:Bindings.ev_add
       ~fflags:Unsigned.UInt32.zero ~data:Intptr.zero ~udata:Uintptr.zero
   in
   let res =
