7c66f8ff23dd4b3a333828bbe9c40b8f7f355ed2
Anurag Soni <anurag@sonianurag.com>
2020-08-21 21:06:27 -0400

    use an error type + result

--- c/stubgen/generate_kqueue_stubs_ml.ml
+++ c/stubgen/generate_kqueue_stubs_ml.ml
@@ -1,4 +1,4 @@
 let () =
-  Cstubs.write_ml ~concurrency:Cstubs.unlocked Format.std_formatter
-    ~prefix:Sys.argv.(1)
+  Cstubs.write_ml ~errno:Cstubs.return_errno ~concurrency:Cstubs.unlocked
+    Format.std_formatter ~prefix:Sys.argv.(1)
     (module Kqueue_stubs.Definition)
