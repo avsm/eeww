9bd0644064f679b200bb1d1c849f76f90a6e1eb4
Jules Aguillon <juloo.dsi@gmail.com>
2021-03-15 19:15:09 +0100

    Remove mention of x84 and x64 in code and file names

--- src/select_suffix.ml
+++ src/select_suffix.ml
@@ -1,24 +1,14 @@
 let invalid_arg fmt = Format.ksprintf (fun s -> invalid_arg s) fmt
 
-let load_file path =
-  let ic = open_in path in
-  let n = in_channel_length ic in
-  let s = Bytes.create n in
-  really_input ic s 0 n ; close_in ic ; Bytes.unsafe_to_string s
-
-let store_file path contents =
-  let oc = open_out path in
-  output_string oc contents ; close_out oc
-
 let () =
-  let is_x64 =
-    let usage () = invalid_arg "%s --x64 (true|false) -o <output>" Sys.argv.(0) in
+  let is_64bit =
+    let usage () = invalid_arg "%s --sixtyfour (true|false)" Sys.argv.(0) in
     match Sys.argv with
-    | [|_; "--x64"; ("true" | "!true")|] -> true
-    | [|_; "--x64"; ("false" | "!false")|] -> false
-    | [|_; "--x64"; v |] -> invalid_arg "Invalid argument of x64 option: %s" v
+    | [|_; "--sixtyfour"; ("true" | "!true")|] -> true
+    | [|_; "--sixtyfour"; ("false" | "!false")|] -> false
+    | [|_; "--sixtyfour"; v |] -> invalid_arg "Invalid argument of sixtyfour option: %s" v
     | _ -> usage ()
     | exception _ -> usage ()
   in
-  let suffix = if is_x64 then "_x64_backend.ml" else "_x86_backend.ml" in
+  let suffix = if is_64bit then "_64_backend.ml" else "_32_backend.ml" in
   print_string suffix
