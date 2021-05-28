d72d69132f28a6e622ad83cff3e21f84662e9d26
Patrick Ferris <pf341@patricoferris.com>
2021-05-29 00:53:23 +0100

    blocks register if in thread

--- test/main.ml
+++ test/main.ml
@@ -6,8 +6,9 @@ let () =
   let block1 = Dispatch.Block.create (fun () -> print_endline "Hello") in
   let block2 = Dispatch.Block.create (fun () -> print_endline "World") in
   Dispatch.Block.exec block1;
-  Dispatch.Block.exec block2;
-  let d1 = Dispatch.Data.create (Cstruct.to_bigarray (Cstruct.of_string "hello1")) in 
-  let d2 = Dispatch.Data.create (Cstruct.to_bigarray (Cstruct.of_string "hello2")) in 
-  let d3 = Dispatch.Data.concat d1 d2 in 
-  Dispatch.Data.apply (fun d -> print_int (Dispatch.Data.size d)) d3
+  Dispatch.Block.exec block2
+
+(* let d1 = Dispatch.Data.create (Cstruct.to_bigarray (Cstruct.of_string "hello1")) in
+   let d2 = Dispatch.Data.create (Cstruct.to_bigarray (Cstruct.of_string "hello2")) in
+   let d3 = Dispatch.Data.concat d1 d2 in
+   Dispatch.Data.apply (fun d -> print_int (Dispatch.Data.size d)) d3 *)
