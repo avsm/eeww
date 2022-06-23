ac9c8fd22092b1b2e9dcfaa1c10568b34700b45e
KC Sivaramakrishnan <kc@kcsrk.info>
2022-06-23 13:11:13 +0530

    Updates

--- lib/hdr_histogram.ml
+++ lib/hdr_histogram.ml
@@ -4,7 +4,7 @@ module Types = Types_generated
 
 type t = Types.hdr_histogram structure ptr
 
-let hdr_init ~lowest_discernible_value ~highest_trackable_value
+let init ~lowest_discernible_value ~highest_trackable_value
              ~significant_figures =
   let h = allocate (ptr Types.hdr_histogram)
                    (from_voidp (Types.hdr_histogram) null)
@@ -17,9 +17,9 @@ let hdr_init ~lowest_discernible_value ~highest_trackable_value
   let h' : t = !@ h in
   h'
 
-let hdr_record_value h v = C.Function.hdr_record_value h (Int64.of_int v)
+let record_value h v = C.Function.hdr_record_value h (Int64.of_int v)
 
-let hdr_close h = C.Function.hdr_close h
+let close h = C.Function.hdr_close h
 
-let hdr_value_at_percentile h p =
+let value_at_percentile h p =
   Int64.to_int @@ C.Function.hdr_value_at_percentile h p
--- test/test1.ml
+++ test/test1.ml
@@ -3,24 +3,24 @@ open Hdr_histogram
 let print_percentiles h =
   let percentiles = [| 50.0; 75.0; 90.0; 99.0; 99.9; 99.99; 99.999; 100.0 |] in
   Fun.flip Array.iter percentiles (fun p ->
-    Printf.printf "%f \t %d\n" p (hdr_value_at_percentile h p))
+    Printf.printf "%f \t %d\n" p (value_at_percentile h p))
 
 let main () =
-  let h = hdr_init ~lowest_discernible_value:1
+  let h = init ~lowest_discernible_value:1
                   ~highest_trackable_value:100_000_000
                   ~significant_figures:3
   in
   for i=1000 to 100_000 do
     for _j =1 to 1000 do
-      ignore @@ hdr_record_value h i
+      ignore @@ record_value h i
     done;
   done;
   for i=100_001 to 110_000 do
     for _j = 1 to 100 do
-      ignore @@ hdr_record_value h i
+      ignore @@ record_value h i
     done;
   done;
   print_percentiles h;
-  hdr_close h
+  close h
 
 let _ = main ()
