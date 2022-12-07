00f21a801d7dc858bf5a0564817c7c1d21d0dbbf
Anil Madhavapeddy <anil@recoil.org>
2022-12-07 14:16:52 +0000

    remove ocaml 5.0 vendor

--- boot/ocaml/testsuite/tests/load_path/test.ml
+++
@@ -1,9 +0,0 @@
-(* TEST
-
-subdirectories = "test"
-
-* setup-ocaml-build-env
-** ocaml
-test_file = "test/driver.ml"
-ocaml_script_as_argument = "true"
-*)
--- boot/ocaml/testsuite/tests/typing-missing-cmi-3/original.ml
+++
@@ -1,8 +0,0 @@
-type 'a t = T
-module type T = sig type t end
-
-type r = { x:unit }
-let r = { x = () }
-
-type s = S
-let s = S
