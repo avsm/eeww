bd668c41b5b84b0af093151091e8e91a1726581f
Anil Madhavapeddy <anil@recoil.org>
2022-12-21 21:51:05 +0000

    fetch: wip

--- src/fetch.ml
+++ src/fetch.ml
@@ -1,27 +1,34 @@
 open Cohttp_eio
 open Eio
 
+(** [Conn] manages TCP/Unix domain connections *)
 module Conn = struct
+
+  (** Conn scheduler *)
   module S = struct
     type t = {
       label: string;
       net: Eio.Net.t;
     }
-    let v ~label ~net = { label; net }
+
+    let v ~label ~net =
+      traceln "Conn.S: init %s" label;
+      { label; net }
+
     let _pp fmt t = Fmt.str fmt "%s" t.label
   end
 
-  let make ~s ~sw stream =
+  let make s ~sw stream =
+    traceln "Conn.make: %s -> %a" s.S.label Net.Sockaddr.pp stream;
+    (* TODO connection pooling here *)
     Net.connect ~sw s.S.net stream
 end
 
-module Dns = struct
-
-end
-
+(** [Url] fetches Uri bodies *)
 module Url = struct
 
   module Https = struct
+
     module S = struct
       type t = {
         label: string;
@@ -37,13 +44,13 @@ module Url = struct
     let _pp fmt t = Fmt.str fmt "%s" t.label
     end
 
-    let fetch ~s (base, resource) =
+    let fetch s (base, resource) =
       let headers = None in
       match Eio.Net.getaddrinfo_stream ~service:"https" s.S.net base with
       | [] -> failwith "host resolution failed"
       | stream :: _ ->
           Switch.run @@ fun sw -> (* XXX move *)
-          let conn = Conn.make ~s:s.S.conns ~sw stream in
+          let conn = Conn.make s.S.conns ~sw stream in
           let host = Domain_name.(of_string_exn base |> host |> Result.to_option) in
           let conn = Tls_eio.client_of_flow ?host s.S.tls_config conn in
           let resp = Client.get ?headers ~conn (base, None) resource in
@@ -56,27 +63,30 @@ module Url = struct
       https: Https.S.t;
       (* TODO protocols *)
     }
-    let v ~label ~https = { label; https } (* TODO dynamic protocal reg *)
+    let v ~label ~net =
+      Conn.S.v ~label:"conns" ~net |> fun conns ->
+      Https.S.v ~label:"https" ~net ~conns |> fun https ->
+      {label; https}
+
     let _pp fmt t = Fmt.str fmt "%s" t.label
   end
   
-  let fetch ~s uri =
+  let fetch s uri =
     match Uri.scheme uri with
-    | Some "https" -> Https.fetch ~s:s.S.https ((Option.get (Uri.host uri)), (Uri.path uri))
+    | Some "https" ->
+        Https.fetch s.S.https ((Option.get (Uri.host uri)), (Uri.path uri))
     | _ -> failwith "unknown schema"
 end
 
 let _ =
-
   (* the scheduler setup *)
   Eio_main.run @@ fun env ->
   Eio.Ctf.with_tracing @@ fun () ->
   Mirage_crypto_rng_eio.run (module Mirage_crypto_rng.Fortuna) env @@ fun () ->
-  Conn.S.v ~label:"conns" ~net:env#net |> fun conns ->
-  Url.Https.S.v ~label:"https" ~net:env#net ~conns |> fun https ->
-  Url.S.v ~label:"url" ~https |> fun url_s ->
+  (* TODO turn this into handler *)
+  Url.S.v ~label:"url" ~net:env#net |> fun url_s ->
 
   (* start of app code *)
   let url = Uri.of_string "https://anil.recoil.org/" in
-  let body = Url.fetch ~s:url_s url  in
-  Eio.traceln "body: %s" body
+  let body = Url.fetch url_s url  in
+  Eio.traceln "body: %d" (String.length body)
