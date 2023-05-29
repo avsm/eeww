bbc773f4b922ef973b8a0ee0ad2200cbe8dc69d7
Anil Madhavapeddy <anil@recoil.org>
2023-05-29 19:16:23 +0100

    capnp-rpc: todo on keepalive

--- lib/capnp-rpc/unix/network.ml
+++ lib/capnp-rpc/unix/network.ml
@@ -84,10 +84,13 @@ let connect net ~sw ~secret_key (addr, auth) =
       | `Unix _ -> ()
       | `TCP _ ->
         (* TODO: check it's OK to set keep-alives after connecting *)
+(* TODO add keepalive into eio *)
+(*
         let fd = Eio_unix.Resource.fd_opt socket |> Option.get in
         Eio_unix.Fd.use_exn "keepalive" fd (fun socket ->
           Unix.setsockopt socket Unix.SO_KEEPALIVE true;
           Keepalive.try_set_idle socket 60)
+*) ()
     end;
     Tls_wrapper.connect_as_client socket secret_key auth
   | exception ex ->
