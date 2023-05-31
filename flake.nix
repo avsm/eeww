cd92d7431493ab1ee4ad5ca49f4392b8bb73fd87
Ryan Gibb <ryan@freumh.org>
2023-05-31 12:14:58 +0100

    added ocaml-lsp-server from opam-nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,13 @@
 {
   description = "Experimental effects-based wonderful webserver";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs.opam-nix.url = "github:tweag/opam-nix";
+
+  outputs = { self, nixpkgs, flake-utils, opam-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        opam-nix-lib = opam-nix.lib.${system};
         depexts = with pkgs; [
           # for libhdr_histogram
           cmake
@@ -24,8 +27,21 @@
             cp _build/default/src/main.exe $out/
           '';
         };
-        devShells.default = pkgs.mkShell {
-          buildInputs = depexts;
-        };
+        devShells.default =
+          let scope = opam-nix-lib.queryToScope {} {
+            ocaml-base-compiler = "5.0.0";
+            dune = "3.7.0";
+            ocaml-lsp-server = "1.15.0-5.0";
+          };
+          in
+          pkgs.mkShell {
+            buildInputs =
+                [
+                  scope.ocaml
+                  scope.dune
+                  scope.ocaml-lsp-server
+                ]
+                ++ depexts;
+          };
       });
 }
